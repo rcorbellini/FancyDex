383f655d6b4a8c67864566d71288553d31e28b47
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2021-01-18 00:08:41 -0300

    fix(base-func): Add func  get by Id on PokemonRepository interface

--- lib/domain/repositories/pokemon_repository.dart
+++ lib/domain/repositories/pokemon_repository.dart
@@ -3,6 +3,7 @@ import 'package:fancy_dex/domain/models/pokemon_model.dart';
 
 abstract class PokemonRepository {
   Future<Either<int, PokemonModel>> getPokemonByName(String name);
+  Future<Either<int, PokemonModel>> getPokemonById(int id);
   Future<Either<int, PokemonModel>> getAllPaged(
       {int offset = 0, int limit = 20});
 }

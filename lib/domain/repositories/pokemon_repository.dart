8f7c57088afb24191b7ce2d683a5583acc8cb202
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2021-01-18 00:06:28 -0300

    feat(base-funcs): Add Interface Repository

---
+++ lib/domain/repositories/pokemon_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fancy_dex/domain/models/pokemon_model.dart';
+
+abstract class PokemonRepository {
+  Future<Either<int, PokemonModel>> getPokemonByName(String name);
+  Future<Either<int, PokemonModel>> getAllPaged(
+      {int offset = 0, int limit = 20});
+}

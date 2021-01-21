0352e6e41739bab07de92cfbb1d09ac64ce53ef5
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2021-01-20 23:15:12 -0300

    fix(getAllpokemon): fix the returning type Pokemon -> List<Pokemon>

--- lib/domain/repositories/pokemon_repository.dart
+++ lib/domain/repositories/pokemon_repository.dart
@@ -5,6 +5,6 @@ import 'package:fancy_dex/domain/models/pokemon_model.dart';
 abstract class PokemonRepository {
   Future<Either<Error, PokemonModel>> getPokemonByName(String name);
   Future<Either<Error, PokemonModel>> getPokemonById(int id);
-  Future<Either<Error, PokemonModel>> getAllPaged(
+  Future<Either<Error, List<PokemonModel>>> getAllPaged(
       {int offset = 0, int limit = 20});
 }
